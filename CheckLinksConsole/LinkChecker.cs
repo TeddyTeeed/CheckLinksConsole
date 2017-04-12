78fcace15da4df056a6a1b0cb17d5b2ab6e7935e
tmeng <teddy.meng@edenred.com>
2017-04-12 22:43:51 +0800

    initial commit

---
+++ CheckLinksConsole/LinkChecker.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CheckLinksConsole
+
+{
+    public class LinkChecker
+    {
+        public static IEnumerable<string> GetLinks (string page)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(page);
+            var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]")
+                .Select(n => n.GetAttributeValue("href", string.Empty))
+                .Where(l => !string.IsNullOrEmpty(l))
+                .Where(l => l.StartsWith("http"));
+            return links;
+        }
+    }
+}
